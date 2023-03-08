b8177dbd38575bafac52bb89d2aeb4890b50c325
katene30 <katenejack@gmail.com>
2023-03-08 17:54:23 +1300

    added listblock for cards

--- streamfields/blocks.py
+++ streamfields/blocks.py
@@ -1,4 +1,5 @@
 from wagtail.core import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 
 class TitleAndTextBlock(blocks.StructBlock):
@@ -9,6 +10,26 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "title_and_text_block.html"
         icon = "edit"
         label = "Title & Text"
+
+class CardBlock(blocks.StructBlock):
+    title = blocks.CharBlock(required=True, help_text='Add your title')
+
+    cards = blocks.ListBlock(
+        blocks.StructBlock(
+            [
+                ("image", ImageChooserBlock(required=True)),
+                ("title", blocks.CharBlock(required=True, max_length=40)),
+                ("text", blocks.TextBlock(required=True, max_length=200)),
+                ("button_page", blocks.PageChooserBlock(required=False)),
+                ("button_url", blocks.URLBlock(required=False, help_text="If the button page above is selected, that will be used first.")),
+            ]
+        )
+    )
+    class Meta:
+        template = "card_block.html"
+        icon = "placeholder"
+        label = "Staff Cards"
+
 
 class RichTextBlock(blocks.RichTextBlock):
     class Meta:
